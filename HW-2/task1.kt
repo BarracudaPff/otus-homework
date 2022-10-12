479da65a3387dc189fe50248770bc0d4f52d7862
Sergey Kudashkin <ya.sikuda@yandex.ru>
2022-10-12 14:33:04 +0300

    task1

---
+++ HW-2/task1.kt
@@ -0,0 +1,32 @@
+import java.util.Scanner
+
+fun main(){
+    val scan = Scanner(System.`in`)
+
+    println("ВВедите чиссло:")
+    val inputNumber = scan.nextInt()
+
+    //Проверка на трехзначность
+    if( inputNumber > 999 || inputNumber < 100) {
+        println("Число не трехзначное")
+        return
+    }
+
+    //Расчет суммы цифр
+    val result = sumOfDigits1(inputNumber)
+    println("Сумма цифр равна =  $result")
+
+    //simple test
+    assert(sumOfDigits1(843) == 15)
+}
+
+fun sumOfDigits1(value: Int): Int{
+    var result = 0
+    var handle = value
+
+    while( handle > 0){
+        result += handle%10
+        handle /= 10
+    }
+    return result
+}
